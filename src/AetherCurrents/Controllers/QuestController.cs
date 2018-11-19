8c7cd8a6d1e3aa79f0c49f26fd2cdb17a6ebbc42
Rafael O'Neill <rafael.oneill@gmail.com>
2018-11-19 17:29:56 -0500

    Added QuestController and tests

---
+++ src/AetherCurrents/Controllers/QuestController.cs
@@ -0,0 +1,41 @@
+using AetherCurrents.Database.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AetherCurrents.Controllers
+{
+    /// <summary>
+    /// Handles requests for Quest related data
+    /// </summary>
+    [ApiController]
+    [Route("aethercurrents/[controller]")]
+    public class QuestController : ControllerBase
+    {
+        private readonly AetherCurrentsContext _context;
+
+        /// <summary>
+        /// The constructor. Sets up the dbContext to use.
+        /// </summary>
+        /// <param name="context">The dbContext.</param>
+        public QuestController(AetherCurrentsContext context)
+        {
+            _context = context;
+        }
+        
+        /// <summary>
+        /// Gets a quest by id.
+        /// GET aethercurrents/quest/1
+        /// </summary>
+        /// <param name="id">The quest id.</param>
+        /// <returns>The quest represented by the id, if found.</returns>
+        [HttpGet("{id}")]
+        public ActionResult<Quest> Get(int id)
+        {
+            var quest = _context.Quest.Find(id);
+            if (quest == null)
+            {
+                return NotFound();
+            }
+            return quest;
+        }
+    }
+}
---
+++ tests/AetherCurrents.Tests/Tests/QuestControllerTest.cs
@@ -0,0 +1,74 @@
+using AetherCurrents.Controllers;
+using AetherCurrents.Database.Entities;
+using AutoFixture;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+
+namespace AetherCurrents.Tests
+{
+    /// <summary>
+    /// Set of tests for the QuestController
+    /// </summary>
+    public class QuestControllerTest : IDisposable
+    {
+        private QuestController _controller;
+        private AetherCurrentsContext _context;
+        private Fixture _fixture;
+
+        /// <summary>
+        /// Test setup
+        /// </summary>
+        public QuestControllerTest()
+        {
+            _fixture = new Fixture();
+            var options = new DbContextOptionsBuilder<AetherCurrentsContext>()
+                .UseInMemoryDatabase(databaseName: "quest").Options;
+            _context = new AetherCurrentsContext(options);
+            _controller = new QuestController(_context);
+        }
+
+        /// <summary>
+        /// Test teardown
+        /// </summary>
+        public void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+
+        /// <summary>
+        /// Tests that the Get method returns data
+        /// </summary>
+        [Trait("Area", "Api")]
+        [Trait("Domain", "Controller")]
+        [Trait("Controller", "Quest")]
+        [Fact]
+        public void Get_WhenDataAvailable_ReturnsData()
+        {
+            var quest = _fixture.Create<Quest>();
+            _context.Quest.Add(quest);
+            _context.SaveChanges();
+
+            var results = _controller.Get(quest.Id).Result as OkObjectResult;
+
+            Assert.IsType<OkObjectResult>(results);
+            Assert.Equal(quest, results.Value);
+        }
+
+        /// <summary>
+        /// Tests that the Get method returns NotFound
+        /// </summary>
+        [Trait("Area", "Api")]
+        [Trait("Domain", "Controller")]
+        [Trait("Controller", "Quest")]
+        [Fact]
+        public void Get_WhenNoDataAvailable_ReturnsNotFound()
+        {
+            var results = _controller.Get(1);
+
+            Assert.IsType<NotFoundResult>(results.Result);
+        }
+    }
+}
